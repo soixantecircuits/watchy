5fbc4072cd87286b5c4eee4ef4dc2be8d7e5535b
hugo <hugo@soixantecircuits.fr>
2015-02-12 12:13:25 +0100

    remove old file

--- test.js
+++
@@ -1,8 +0,0 @@
-var config = "/home/hugo/sources/python/pyying/snaps",
-    watch = require('watch');
-
-watch.createMonitor(config, function (monitor) {
-    monitor.on("created", function (path, stat) {
-        console.log("created", path);
-    });
-});
